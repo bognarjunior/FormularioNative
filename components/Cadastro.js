4f013f3709f78edbcda06651b9c3634d862de643
Bognar Junior <bognar_junior@yahoo.com.br>
2018-08-04 16:41:24 -0300

    Ajustando componentes
    Ajustando os componentes e limpando formulário

--- components/Cadastro.js
+++ components/Cadastro.js
@@ -1,41 +1,64 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 export default class Cadastro extends Component {
   state = {
     email: '',
     password: '',
+    msg: null,
   };
 
+  renderMsg = () => {
+    if (this.state.msg) {
+      return <Text style={styles.textMsg}>{this.state.msg}</Text>
+    }
+  }
+
+  onCadastrarPress =() => {
+    this.setState({ 
+      msg: 'Muito obrigado por se cadastrar, verifique seu email os próximos passos',
+      email: '',
+      password: '',
+    })
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.title}>
-          <Text style={styles.titleLabel}> Nova Conta </Text>
-        </View>
-        <View style={styles.inputContainer}>
-          <Text style={styles.labelInput}> Email: </Text>
-          <TextInput 
-            style={styles.inputText} 
-            underlineColorAndroid='transparent'
-            value={this.state.email}
-            onChangeText={t => this.setState({ email: t })}
-            autoCapitalize='none'
-            autoCorrect={false}
+        <View>
+          <View style={styles.title}>
+            <Text style={styles.titleLabel}> Nova Conta </Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.labelInput}> Email: </Text>
+            <TextInput 
+              style={styles.inputText} 
+              underlineColorAndroid='transparent'
+              value={this.state.email}
+              onChangeText={t => this.setState({ email: t })}
+              autoCapitalize='none'
+              autoCorrect={false}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.labelInput}> Senha: </Text>
+            <TextInput 
+              style={styles.inputText} 
+              underlineColorAndroid='transparent'
+              value={this.state.password}
+              onChangeText={t => this.setState({ password: t })}
+              autoCapitalize='none'
+              autoCorrect={false}
+              secureTextEntry={true}
+            />
+          </View>
+          
+          <Button 
+            title='Cadastrar' 
+            onPress={this.onCadastrarPress}
           />
         </View>
-        <View style={styles.inputContainer}>
-          <Text style={styles.labelInput}> Senha: </Text>
-          <TextInput 
-            style={styles.inputText} 
-            underlineColorAndroid='transparent'
-            value={this.state.password}
-            onChangeText={t => this.setState({ password: t })}
-            autoCapitalize='none'
-            autoCorrect={false}
-            secureTextEntry={true}
-          />
-        </View>
+        {this.renderMsg()}
       </View>
     )
   }
@@ -44,6 +67,7 @@ export default class Cadastro extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'space-between',
   },
   title: {
     backgroundColor: '#3399ff',
@@ -74,5 +98,10 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     height: 40,
     padding: 8,
+  },
+  textMsg: {
+    alignSelf: 'center',
+    color: '#33ff33',
+    fontSize: 18,
   }
 });
